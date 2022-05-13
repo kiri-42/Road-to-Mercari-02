00277ecf7218996df3621913b7bd95ee0f75ec46
kiri0314 <t.kirihara0314@gmail.com>
2022-05-14 06:02:41 +0900

    feat: タイマー機能を実装

--- ex00/main.go
+++ ex00/main.go
@@ -1,5 +1,32 @@
 package main
 
+import (
+	"fmt"
+	"time"
+	"strconv"
+)
+
+// 難易度を選択できるようにする
+// 日本語訳を表示するかオプションで指定できる
+// 英単語はランダムにして重複しないようにする
+// エンターを押したらスタートするようにする
+
 func main() {
-	println("main")
+	var c int
+	go func() {
+		for {
+			fmt.Println("aaaa")
+			fmt.Print("-> ")
+			var input string
+			fmt.Scan(&input)
+			if input == "aaaa" {
+				c++
+			}
+		}
+	}()
+
+	select {
+	case <-time.After(5 * time.Second):
+		fmt.Println("\nTime's up! Score: " + strconv.Itoa(c))
+	}
 }
